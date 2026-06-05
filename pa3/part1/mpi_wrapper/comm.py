cd9fca95bbf8b63424987340539dc350e817aea0
Ksheer Sagar Agrawal <ksheer.agrawal@iitgn.ac.in>
2026-06-05 13:00:35 -0700

    Complete PA3 implementations and Part 3 sweep

--- pa3/part1/mpi_wrapper/comm.py
+++ pa3/part1/mpi_wrapper/comm.py
@@ -82,16 +82,99 @@ class Communicator(object):
 
     # ---------- optional: paste your PA2 implementations here ----------
     def myAllreduce(self, src_array, dest_array, op=MPI.SUM):
-        """OPTIONAL: paste your PA2 implementation here to use it in this PA."""
-        raise NotImplementedError(
-            "myAllreduce is optional in PA3. Paste your PA2 implementation to enable it."
-        )
+        """Point-to-point all-reduce implementation copied from PA2."""
+        assert src_array.size == dest_array.size
+
+        nprocs = self.comm.Get_size()
+        rank = self.comm.Get_rank()
+        root = 0
+
+        if op not in (MPI.SUM, MPI.MIN, MPI.MAX):
+            raise ValueError("Unsupported reduction op")
+
+        if nprocs > 0 and (nprocs & (nprocs - 1)) == 0:
+            reduced = np.array(src_array, copy=True)
+            recv_buf = np.empty_like(src_array)
+            mask = 1
+
+            while mask < nprocs:
+                peer = rank ^ mask
+                self.comm.Sendrecv(reduced, dest=peer, recvbuf=recv_buf, source=peer)
+                if op == MPI.SUM:
+                    reduced += recv_buf
+                elif op == MPI.MIN:
+                    np.minimum(reduced, recv_buf, out=reduced)
+                else:
+                    np.maximum(reduced, recv_buf, out=reduced)
+                mask <<= 1
+
+            np.copyto(dest_array, reduced)
+        elif rank == root:
+            reduced = np.array(src_array, copy=True)
+            recv_buf = np.empty_like(src_array)
+
+            for src_rank in range(nprocs):
+                if src_rank == root:
+                    continue
+                self.comm.Recv(recv_buf, source=src_rank)
+                if op == MPI.SUM:
+                    reduced += recv_buf
+                elif op == MPI.MIN:
+                    np.minimum(reduced, recv_buf, out=reduced)
+                else:
+                    np.maximum(reduced, recv_buf, out=reduced)
+
+            np.copyto(dest_array, reduced)
+            for dest_rank in range(nprocs):
+                if dest_rank != root:
+                    self.comm.Send(dest_array, dest=dest_rank)
+        else:
+            self.comm.Send(src_array, dest=root)
+            self.comm.Recv(dest_array, source=root)
+
+        src_bytes = src_array.itemsize * src_array.size
+        self.total_bytes_transferred += src_bytes * 2 * (nprocs - 1)
 
     def myAlltoall(self, src_array, dest_array):
-        """OPTIONAL: paste your PA2 implementation here to use it in this PA."""
-        raise NotImplementedError(
-            "myAlltoall is optional in PA3. Paste your PA2 implementation to enable it."
-        )
+        """Point-to-point all-to-all implementation copied from PA2."""
+        nprocs = self.comm.Get_size()
+        rank = self.comm.Get_rank()
+
+        assert src_array.size % nprocs == 0
+        assert dest_array.size % nprocs == 0
+
+        send_count = src_array.size // nprocs
+        recv_count = dest_array.size // nprocs
+        assert send_count == recv_count
+
+        src_flat = src_array.reshape(-1)
+        dest_flat = dest_array.reshape(-1)
+
+        start = rank * send_count
+        end = start + send_count
+        dest_flat[start:end] = src_flat[start:end]
+
+        requests = []
+        for peer in range(nprocs):
+            if peer == rank:
+                continue
+            recv_start = peer * recv_count
+            recv_end = recv_start + recv_count
+            requests.append(self.comm.Irecv(dest_flat[recv_start:recv_end], source=peer))
+
+        for peer in range(nprocs):
+            if peer == rank:
+                continue
+            send_start = peer * send_count
+            send_end = send_start + send_count
+            requests.append(self.comm.Isend(src_flat[send_start:send_end], dest=peer))
+
+        MPI.Request.Waitall(requests)
+
+        send_seg_bytes = src_array.itemsize * send_count
+        recv_seg_bytes = dest_array.itemsize * recv_count
+        self.total_bytes_transferred += send_seg_bytes * (nprocs - 1)
+        self.total_bytes_transferred += recv_seg_bytes * (nprocs - 1)
 
 
 # Default global communicator (mirrors the pa2 convention).
